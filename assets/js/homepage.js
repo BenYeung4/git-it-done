fbb6a7c3bba0c6a27af3acb1a8bdd1dddc9e512a
Ben <ben@Benjamins-MacBook-Pro.local>
2022-05-17 23:53:48 -0400

    6.2.1

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,25 @@
+var getUserRepos = function(user){
+  //format the gitub api url
+  var apiUrl = "https://api.github.com/user/" + user + "/repos";
+
+  //make a request to the url
+  fetch(apiUrl).then(function(response){
+    response.json().then(function(data){
+      console.log(data);
+    });
+  });
+};
 
 
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-  };
+
+
+// var response = fetch("https://api.github.com/users/octocat/repos").then(function(response){
+//   response.json().then(function(data){
+//     console.log(data);
+//   });
+// });
+
+//console.log(response);
+//console.log("inside", response);
+//console.log("outside");
+
